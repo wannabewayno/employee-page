1054e90529b26b0253de12efb7d422b0c3197299
Wayne <wayne.c.griffiths@gmail.com>
2020-07-01 11:22:22 +0800

    FIX box-shadow layering issue

--- src/components/lib/listElements/employee/index.jsx
+++ src/components/lib/listElements/employee/index.jsx
@@ -7,18 +7,20 @@ const { name, role, email, department, phone, image } = props.employee;
     return (
         <li className='listEmployee'>
             <main>
-            	<img src={image} alt={`${name}`}/>
-            	<section>
-            	    <h6 className="name">{name}</h6>
-            	    <hgroup>
-                        <h6>Department: {department}</h6>
-            	    	<h6>Email: {email}</h6>            	    	
-            	    	<h6>Extension: {phone}</h6>
-            	    </hgroup>  
-            	</section>
-            	<div className="colourTab"></div>
+            	<main>
+                    <img src={image} alt={`${name}`}/>
+                    <section>
+                        <h6 className='name'>{name}</h6>
+                        <hgroup>
+                            <h6>Department: {department}</h6>
+                            <h6>Email: {email}</h6>            	    	
+                            <h6>Extension: {phone}</h6>
+                        </hgroup>  
+                    </section>
+                    <div className='colourTab'></div>
+                </main>  
             </main>
-            <h6 className="title">{role}</h6>  
+            <h6 className='title'>{role}</h6>
         </li>
     )
 }
