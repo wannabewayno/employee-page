53180da70c10d72bcb641f280ef2db2bf2de5a9c
Wayne <wayne.c.griffiths@gmail.com>
2020-07-06 00:48:59 +0800

    CLEA UP: removed unused useEffect import in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect } from 'react';
+import React, { useState } from 'react';
 import Nav from './components/elements/Nav';
 import FormContainer from './components/lib/containers/FormContainer';
 import Dropdown from './components/lib/inputs/Dropdown';
